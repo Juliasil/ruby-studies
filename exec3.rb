c24298a7e22b721e0b1f4ae1eae09c8356128752
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-07-10 21:51:17 -0300

    Algoritmo exec 3 feito

---
+++ exec3.rb
@@ -0,0 +1,11 @@
+# Exercicio
+# Dado que eu tenha uma variável com número 704
+# qual seria o resultado da soma da variável b que tem
+# armazenamento 38 somando com a variável anterior.
+
+puts "Digite um numero para a operação matemática de soma com número 38"
+a = gets.to_i
+b = 38
+c = a + b
+
+puts "O resultado da operação é: #{c}"
