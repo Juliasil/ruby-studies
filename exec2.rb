b01d395d56b6bf9a32b3000c201832d457d3d51a
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-07-10 21:21:03 -0300

    Feito o algoritmo exec 2

---
+++ exec2.rb
@@ -0,0 +1,11 @@
+# Exercicio
+# Dado que eu tenha uma variável com número 455
+# qual seria o resultado da soma da variável b que tem
+# armazenamento 68 somando com a variável anterior.
+
+
+a = 455
+b = 68
+a = a + b
+
+puts a
