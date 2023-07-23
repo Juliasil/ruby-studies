b8ead4f467c2309db4dfd5ea261701060f055a4c
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-07-23 13:10:17 -0300

    Adicionando o Gemfile

---
+++ doutor-renato.rb
@@ -0,0 +1,8 @@
+# se renato é um dentista e ele precisa de um software que faça o calculo
+# da porcentagem de desconto que ela dará para os seus clientes no valor
+# total tratamento, faça um software que solicite o valor total do tratamento
+# sollicite o valor da porcentagem e logo após mostre o valor com desconto
+# e o valor descontado
+
+# Renato ficará MUITO feliz se vc resolver o problema  dele.
+          
---
+++ exec4.rb
@@ -0,0 +1,3 @@
+a = "Juliana"
+
+puts "O valor da variavel a é " + a
