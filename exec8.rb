035b987013c10f27317ac51101677626a9005583
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-07-26 22:45:43 -0300

    resolvendo algumas exerc de strings

---
+++ exec8.rb
@@ -0,0 +1,34 @@
+require 'byebug'
+debugger
+#a = '   REVISÃO de hoje    '
+#puts a.reverse
+#
+# a = '    REVISÃO de hoje    '
+# puts a.include?("REVISÂO")
+#
+# a = '    REVISÃO de hoje    '  + "---"
+# puts a.rstrip
+# puts a.rstrip + "---"
+
+#a = '    REVISÃO de hoje    '
+# puts a.rstrip
+#
+# a = '    REVISÃO de hoje    '
+# puts a.lstrip
+#
+# a = '    REVISÃO de hoje    '
+# puts a.strip
+#
+# a = 'revisão de hoje'
+# puts a[0, 7]
+#
+# b = '---3'
+# a = 'revisão de hoje'
+#
+# a << b
+#
+# puts a
+#
+ b = 1
+ a = "revisão de hoje " + b.to_s
+puts a
