d5043d72c7e5e9e3df0d89455399b14246bd9719
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-08-11 16:30:53 -0300

    Inicializado o exercício com super

---
+++ poo/super.rb
@@ -0,0 +1,20 @@
+class Franquia
+  def descricao
+    "Franquia!"
+  end
+end
+
+class Franqueado  < Franquia
+  def descricao
+    puts super
+    "Franqueado!"
+  end
+end
+puts "================================="
+
+f = Franquia.new
+puts f.descricao
+
+puts "=================================="
+ff = Franqueado.new
+puts ff.descricao
