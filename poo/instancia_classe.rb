dc4cc0c2a254eb5e5098519826f58858e0354abc
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-08-11 22:33:54 -0300

    Exercício de métodos de intância de classe

---
+++ poo/instancia_classe.rb
@@ -0,0 +1,18 @@
+class Teste   # Método de Instãncia
+  def oi
+    "Oi!"
+  end
+
+  def self.hello  # Método de Classe
+    "Hello!"
+  end
+end
+
+
+
+#t = Teste.new
+#puts t.oi
+
+puts Teste.hello
+
+
