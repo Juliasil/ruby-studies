6f63dab31b89988432a6287652972502abb6136f
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-08-11 22:17:14 -0300

    Exercício de exemplo de self

---
+++ poo/self.rb
@@ -0,0 +1,21 @@
+class Exemplo
+  def meu_self
+    puts "Esse é meu self: #{self}"
+  end
+end
+
+class Exemplo1
+  def meu_self
+    puts "Esse é meu self: #{self}"
+  end
+end
+
+e = Exemplo.new
+e.meu_self
+
+e1 = Exemplo1 .new
+e1.meu_self
+
+
+
+
