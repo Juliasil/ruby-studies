677348ae0ab59269538ed837237716a08a0a340c
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-08-15 06:50:02 -0300

    Lógica exercício de porcentagem

--- logica_ruby/doutor-renato.rb
+++ logica_ruby/doutor-renato.rb
@@ -5,4 +5,20 @@
 # e o valor descontado
 
 # Renato ficará MUITO feliz se vc resolver o problema  dele.
-          
+system 'clear'
+
+puts "olá seu Renato tudo bem?"
+puts "Digite aqui o valor total do pedido: "
+valor_total = gets.to_f
+puts "Digite a porcentagem de desconto:"
+porcentagem_desconto = gets.to_f
+
+valor_do_desconto =  valor_total * porcentagem_desconto / 100
+valor_total_com_desconto = valor_total - valor_do_desconto
+
+puts "Olá Seu Renato"
+puts "O valor total digitado foi: #{valor_total}"
+puts "O valor de desconto foi: #{valor_do_desconto}"
+puts "======================================"
+puts "Valor total a ser cobrado é de: #{valor_total_com_desconto}"
+puts "================================="
